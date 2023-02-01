1839102151b9c3bfff7f1fef352f8f4886e8b6c7
jcammas <matoulet.cammas@gmail.com>
2023-02-01 17:20:09 +0100

    add : share image (little bug to fix) & download image on the device + little update in images provider

--- my_app/lib/view/home/widget/image_card.dart
+++ my_app/lib/view/home/widget/image_card.dart
@@ -1,11 +1,61 @@
-// ignore_for_file: avoid_print
+// ignore_for_file: unused_local_variable
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:flutter/services.dart';
 
-class ImageCard extends StatelessWidget {
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+
+class ImageCard extends StatefulWidget {
   final String url;
 
   const ImageCard({super.key, required this.url});
+
+  @override
+  State<ImageCard> createState() => _ImageCardState();
+}
+
+class _ImageCardState extends State<ImageCard> {
+  bool isDownloading = false;
+
+  static const snackBar = SnackBar(
+    content: Text('Successful download.'),
+  );
+
+  static const snackBarError = SnackBar(
+    content: Text('Download failed.'),
+  );
+
+  void shareImage(String url) async {
+    await Share.share(
+      url,
+    );
+  }
+
+  Future<void> _downloadImage(String url) async {
+    setState(() {
+      isDownloading = true;
+    });
+    Dio dio = Dio();
+    try {
+      Response response = await dio.get(url,
+          options: Options(responseType: ResponseType.bytes));
+      final result = await ImageGallerySaver.saveImage(
+        Uint8List.fromList(response.data),
+        quality: 100,
+      );
+      setState(() {
+        isDownloading = false;
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      });
+    } catch (e) {
+      setState(() {
+        isDownloading = false;
+        ScaffoldMessenger.of(context).showSnackBar(snackBarError);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,11 +77,11 @@ class ImageCard extends StatelessWidget {
                 decoration: const BoxDecoration(color: Colors.white),
                 child: InkWell(
                   onTap: () async {
-                    Object arguments = {"url": url};
+                    Object arguments = {"url": widget.url};
                     String query = '/fullScreen';
                     Navigator.pushNamed(context, query, arguments: arguments);
                   },
-                  child: Image.network(url, fit: BoxFit.fill),
+                  child: Image.network(widget.url, fit: BoxFit.fill),
                 )),
             Padding(
               padding: const EdgeInsets.all(10.0),
@@ -42,9 +92,7 @@ class ImageCard extends StatelessWidget {
                   Align(
                     alignment: Alignment.topRight,
                     child: IconButton(
-                      onPressed: () {
-                        print("like");
-                      },
+                      onPressed: () {},
                       icon: const Icon(
                         Icons.favorite_border,
                         size: 25,
@@ -55,8 +103,8 @@ class ImageCard extends StatelessWidget {
                   Align(
                     alignment: Alignment.topRight,
                     child: IconButton(
-                      onPressed: () {
-                        print("download");
+                      onPressed: () async {
+                        _downloadImage(widget.url);
                       },
                       icon: const Icon(
                         Icons.download,
@@ -68,8 +116,8 @@ class ImageCard extends StatelessWidget {
                   Align(
                     alignment: Alignment.topRight,
                     child: IconButton(
-                      onPressed: () {
-                        print("share");
+                      onPressed: () async {
+                        shareImage(widget.url);
                       },
                       icon: const Icon(
                         Icons.share,
